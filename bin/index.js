53a2de003356c93920166efc2f7fc7f45118d654
Maxime BAUMANN <maxime.baumann@psycle.io>
2022-11-25 13:50:45 +0100

    Clean some dirs

--- bin/index.js
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env node
-'use strict';
-require('./cli').default(process.argv);
-//# sourceMappingURL=index.js.map
--- src/commands/update.ts
+++ src/commands/update.ts
@@ -1,9 +1,17 @@
+import type { Command } from 'commander'
+
 type UpdateCommandOptions = {
     readonly skipPrompt: boolean
 }
-export default async function update(source: string, destinations: string[], option: UpdateCommandOptions) {
+export default async function update(
+    source: string,
+    destinations: string[],
+    option: UpdateCommandOptions,
+    command: Command,
+) {
     console.log('Source : ' + source)
     console.log('Dest : ' + destinations.concat(', '))
     console.log('Option : ' + JSON.stringify(option))
+    console.log('Command : ' + command.name())
     process.exit(0)
 }
